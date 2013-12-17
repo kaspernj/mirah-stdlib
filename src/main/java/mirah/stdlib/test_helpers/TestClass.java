45c098a49cd5ba0813278fb0d0427d73cfc7e555
kaspernj <k@spernj.org>
2013-12-17 22:57:36 +0100

    Added comment so it is logical what this empty class is for.

--- src/main/java/mirah/stdlib/test_helpers/TestClass.java
+++ src/main/java/mirah/stdlib/test_helpers/TestClass.java
@@ -1,6 +1,4 @@
 package mirah.stdlib.test_helpers;
 
-public class TestClass
-{
-	
-}
+/* This class should be extended in order to let the system know what to scan for test-methods. */
+public class TestClass{}
